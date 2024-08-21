b53d3ef03b0cc793378a8f4dbc51d9271fb59202
nroberts <120337023+NaeemRoberts@users.noreply.github.com>
2024-08-21 12:09:23 -0400

    Update autoVec.py

--- autoVec.py
+++ autoVec.py
@@ -18,6 +18,7 @@ def processFile(file_path, output_file="eigvec4.dat", fac=241.7991, eig_index=3)
             frequencies = np.array([band['frequency'] for band in bands])
             eigenvectors = np.array([band['eigenvector'] for band in bands])
     except Exception as e:
+        
         return print(f"Error processing file: {e}")
 
     print("\n".join([f"{f/fac*1000:.3g} meV" for f in frequencies]))
@@ -30,6 +31,7 @@ def processFile(file_path, output_file="eigvec4.dat", fac=241.7991, eig_index=3)
                 print(f"{vxr:.4f} {vyr:.4f} {vzr:.4f}")
                 f.write(f"{vxr:.8f} {vyr:.8f} {vzr:.8f}\n")
         print(f"Eigenvector saved to {output_file}")
+        
     except Exception as e:
         print(f"Error saving eigenvector: {e}")
 
