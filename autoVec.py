11f21a2a4720d280e14100ff65ecf2e367278256
nroberts <120337023+NaeemRoberts@users.noreply.github.com>
2024-08-21 12:11:01 -0400

    Update autoVec.py

--- autoVec.py
+++ autoVec.py
@@ -10,7 +10,6 @@ except ImportError:
 def processFile(file_path, output_file="eigvec4.dat", fac=241.7991, eig_index=3):
     if not os.path.exists(file_path):
         return print(f"Error: {file_path} not found.")
-    
     try:
         with open(file_path, "r") as stream:
             phonon = yaml.load(stream, Loader=Loader)
@@ -18,12 +17,10 @@ def processFile(file_path, output_file="eigvec4.dat", fac=241.7991, eig_index=3)
             frequencies = np.array([band['frequency'] for band in bands])
             eigenvectors = np.array([band['eigenvector'] for band in bands])
     except Exception as e:
-        
         return print(f"Error processing file: {e}")
-
     print("\n".join([f"{f/fac*1000:.3g} meV" for f in frequencies]))
     print(f"Eigenvector shape: {eigenvectors.shape}")
-
+    
     try:
         with open(output_file, "w") as f:
             for vec in eigenvectors[eig_index]:
@@ -31,7 +28,6 @@ def processFile(file_path, output_file="eigvec4.dat", fac=241.7991, eig_index=3)
                 print(f"{vxr:.4f} {vyr:.4f} {vzr:.4f}")
                 f.write(f"{vxr:.8f} {vyr:.8f} {vzr:.8f}\n")
         print(f"Eigenvector saved to {output_file}")
-        
     except Exception as e:
         print(f"Error saving eigenvector: {e}")
 
